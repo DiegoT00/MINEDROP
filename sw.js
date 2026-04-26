0f577fd1fbcfc7ee8eae611dee415105260057f0
DiegoT00 <dturresb@gmail.com>
2026-04-26 01:22:37 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'minedrop-v1';
+const FILES = [
+  './minedrop_v9_safari.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(FILES);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
